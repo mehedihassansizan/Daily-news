92167f0e3f9ea9d827b778c10fb45dd92b3c5d06
mehedihassansizan <mahadihassn21777@gmail.com>
2023-06-24 00:49:56 +0600

    first commit

--- js/utilites.js
+++ js/utilites.js
@@ -13,7 +13,12 @@ const displayCatagories = (data) => {
         const newDiv = document.createElement('div');
         newDiv.classList.add('col')
         newDiv.innerHTML = `
-            <h6 onclick="catagoriesItems('${element.category_id}','${element.category_name}')" class="catagories rounded fw-bold">${element.category_name}</h6>
+            <ul onclick="catagoriesItems('${element.category_id}','${element.category_name}'); trogolFuntion('true');" class="navbar nav nav-underline catagories rounded fw-bold d-flex justify-content-center">
+                    <li class="nav-item">
+                        <a class="nav-link news" href="#">${element.category_name}</a>
+                    </li>
+                  </ul>
+            </div>
         `;
         catagoriesDiv.appendChild(newDiv)
     });
@@ -43,29 +48,87 @@ const howManyItems = (items, name) => {
     if (catagoriesLength !== 0) {
         showNewsSection.classList.remove('d-none')
         items.forEach(element => {
-            console.log(element);
+            // console.log(element);
             const showDiv = document.createElement('div')
-            showDiv.className = 'card mb-3 gap-4'
+            showDiv.className = 'card mb-3'
             // showDiv.classList.add()
             showDiv.innerHTML =`
             <div class="row g-0">
-                <div class="col-md-4 p-4">
-                    <img src="${element.thumbnail_url}"  class=" img-fluid rounded-start" alt="...">
+                <div class="col-md-3 d-flex p-2">
+                    <img src="${element.thumbnail_url}" class="img-fluid rounded-start" alt="...">
                 </div>
-            <div class="col-md-8 border">
-                <div class="card-body mx-5 text-left">
-                        <h2 class="card-title fw-bold">${element.title ? element.title: 'NO TITLE FOUND'} </h2>
-                        <p class="card-text ">${element.details.slice(0, 200)}...</p>
+                <div class="col-md-8" style="margin-left: 40px;">
+                    <div class="card-body mt-5 ">
+                            <h2 class="card-title fw-bold">${element.title ? element.title: 'NO TITLE FOUND'} </h2>
+                            <p class="card-text ">${element.details.slice(0, 200)}...</p>
+                    </div>
+                    <div class="d-inline-flex pt-5">
+                        <div class=" d-flex align-items-center">
+                            <a class="navbar-brand p-3" href="#"><img src="${element.author ? element.author.img: 'NO TITLE FOUND'}" alt="Bootstrap" width="40" height="40"></a>
+                            <h6 class="fw-bold mt-2">${element.author ? element.author.name: 'NO TITLE FOUND'} <br> <span class="fw-normal">${element.author ? element.author. published_date: 'NO TITLE FOUND'}</span></h6>
+                        </div>   
+                    </div>
+                    <div class="d-inline-flex px-5">
+                        <div class="">
+                            <i class="fa-solid fa-eye">${element.total_view ? element.total_view: 'NO TITLE FOUND'}</i>
+                        </div>   
+                    </div>
+                    <div class="d-inline-flex px-5">
+                        <div class="">
+                            <i class="fa-solid fa-star"></i>
+                            <i class="fa-solid fa-star"></i>
+                            <i class="fa-solid fa-star"></i>
+                            <i class="fa-solid fa-star-half-stroke"></i>
+                            <i class="fa-regular fa-star"></i>
+                        </div>   
+                    </div>
+                    <div class="d-inline-flex justify-content-end navbar nav nav-underline" style="margin-left: 120px;">
+                        <div  class="nav-item">
+                            <button onclick="newsDetails('${element._id}')" data-bs-toggle="modal" data-bs-target="#staticBackdrop" class="nav-link"><i class="fa-solid fa-arrow-right fa-2xl" style="color: #1266f8;"></i></button>
+                        </div>   
                     </div>
                 </div>
             </div>
+                    
             ` ;
             showNews.appendChild(showDiv)
-
+            trogolFuntion(false)
     });
     } 
     else {
         showNewsSection.classList.add('d-none')
     }
 }
+
+const newsDetails = (id) => {
+    const detailsUrl = `https://openapi.programming-hero.com/api/news/${id}`
+    fetch(detailsUrl)
+    .then(res => res.json())
+    .then(data => displayDetails(data.data))
+    .catch(error => console.log(error))
+}
+const displayDetails = data => {
+    // console.log(data);
+    const dataLength = data.length;
+    data.forEach(element => {
+        console.log(element);
+        const detailstittle = document.getElementById('staticBackdropLabel')
+        detailstittle.innerText =`${element.title}`
+        const detailsBody = document.getElementById('details-body')
+        detailsBody.innerHTML=`
+            <p>${element.details}</p>
+        `
+    });
+}
+
+const trogolFuntion = (data) => {
+    const spinner = document.getElementById('spinner')
+    if(data){
+        spinner.classList.remove('d-none')
+    }
+    else{
+        spinner.classList.add('d-none')
+    }
+}
+
 loadCatagories();
